83f8369e00f2e7fcf6e23ee6728c7b195bf84d72
chimoon168 <54139385+chimoon168@users.noreply.github.com>
2024-03-04 15:51:45 +0800

    Add files via upload

---
+++ files/email/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,[j,k],l,_(m,n,o,p,q,r,s,_(),t,_(u,v,w,x,y,_(z,A,B,C),D,null,E,F,G,F,H,I,J,null,K,L,M,N,O,P,Q,L),R,_(S,_(),T,_()),U,_(),V,_(W,[])),X,_(),Y,_());}; 
+var b="url",c="email.html",d="generationDate",e=new Date(1709286949279.9),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="variables",j="OnLoadVariable",k="SpinnerValue",l="page",m="packageId",n="db467cebac7f4ae6b92e27c3d55e5fd3",o="type",p="Axure:Page",q="name",r="EMAIL",s="notes",t="style",u="baseStyle",v="627587b6038d43cca051c114ac41ad32",w="pageAlignment",x="center",y="fill",z="fillType",A="solid",B="color",C=0xFFFFFFFF,D="image",E="imageHorizontalAlignment",F="near",G="imageVerticalAlignment",H="imageRepeat",I="auto",J="favicon",K="sketchFactor",L="0",M="colorStyle",N="appliedColor",O="fontName",P="Applied Font",Q="borderWidth",R="adaptiveStyles",S="a",T="b",U="interactionMap",V="diagram",W="objects",X="masters",Y="objectPaths";
+return _creator();
+})());
